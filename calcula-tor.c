50d55c3279ae6ab4725181ab5e53485fb61d74ae
Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>
2025-10-08 11:10:33 -0400

    Calcula-tor.c
    Signed-off-by: Finn <114116392+KhajiitHasTheWares@users.noreply.github.com>

---
+++ calcula-tor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <float.h>
+
+int main () {
+  char op;
+double a, b, res;
+
+//reads the operator
+printf("Enter a symbol (+, -, *, /):");
+scanf("%c", &op);
+
+//reads the two numbers entered
+printf("Enter two numbers: ");
+scanf("%lf %lf", &a, &b);
+
+//defines all four operations in switch cases
+switch (op) {
+case '+':
+res = a + b;
+break;
+case '-':
+res = a - b;
+break;
+case '*':
+res = a * b;
+break;
+case '/':
+res = a / b;
+break;
+default:
+printf("Error! Incorrect value! PLease enter another value.\n");
+res = -DBL_MAX
+  }
+if(res!=-DBL_MAX)
+  printf("%.2lf", res);
+
+return 0;
+}
